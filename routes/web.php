61679b8419115f9767e2a22993d6e487a2705351
RYANNNHZ <riantojelek871@gmail.com>
2025-02-04 09:53:10 +0700

    djaru pinjem duit

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use App\Livewire\ListCreate;
 use App\Livewire\Profile;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/',function(){
+    return redirect('/lists');
+});
 Route::get('/lists', Lists::class);
 Route::get('/tasks', Tasks::class);
 Route::get('/listcreate',ListCreate::class);
